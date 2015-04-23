c8ad2920262fe013f9049b5eba793b40a2c92486
Ad Schellevis <ad@opnsense.org>
2015-04-24 00:28:04 +0200

    Revert "tools: apply pf_match.diff"
    This reverts commit db3a0efd14d67a37b4d05f52e910eb42f8663b73.

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -465,13 +465,6 @@ struct pf_osfp_ioctl {
 	int			fp_getnum;	/* DIOCOSFPGET number */
 };
 
-struct pf_rule_actions {
-	u_int16_t	qid;
-	u_int16_t	pqid;
-	u_int32_t	pdnpipe;
-	u_int32_t	dnpipe;
-	u_int8_t	flags;
-};
 
 union pf_rule_ptr {
 	struct pf_rule		*ptr;
@@ -528,8 +521,8 @@ struct pf_rule {
 		u_int32_t		limit;
 		u_int32_t		seconds;
 	}			 max_src_conn_rate;
-	u_int16_t		 qid;
-	u_int16_t		 pqid;
+	u_int32_t		 qid;
+	u_int32_t		 pqid;
 	u_int32_t                dnpipe;
         u_int32_t                pdnpipe;
 #define	PFRULE_DN_IS_PIPE	0x00000010
@@ -773,10 +766,6 @@ struct pf_state {
 	u_int32_t		 creation;
 	u_int32_t	 	 expire;
 	u_int32_t		 pfsync_time;
-	u_int16_t		 qid;
-	u_int16_t		 pqid;
-	u_int32_t		 pdnpipe;
-	u_int32_t		 dnpipe;
 	u_int16_t		 tag;
 	u_int8_t		 log;
 	u_int8_t		 state_flags;
@@ -1148,7 +1137,6 @@ struct pf_pdesc {
 	u_int16_t *sport;
 	u_int16_t *dport;
 	struct pf_mtag	*pf_mtag;
-	struct pf_rule_actions   act;
 
 	u_int32_t	 p_len;		/* total length of payload */
 
