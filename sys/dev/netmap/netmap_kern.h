453fe71bf8e3b3a16b0390aca0bbd27f22415a3b
Franco Fichtner <franco@opnsense.org>
2015-11-20 10:52:00 +0100

    netmap: mute a spammy netmap message

--- sys/dev/netmap/netmap_kern.h
+++ sys/dev/netmap/netmap_kern.h
@@ -163,6 +163,7 @@ struct hrtimer {
 	} while (0)
 
 /* rate limited, lps indicates how many per second */
+#define NRD(lps, format, ...)
 #define RD(lps, format, ...)					\
 	do {							\
 		static int t0, __cnt;				\
