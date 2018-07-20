4cadbef4556b74cbaff40a2b85cae69d99986b2b
Franco Fichtner <franco@opnsense.org>
2018-07-20 10:24:39 +0200

    pf: kernel also handles interface address lookups
    When () is used for interface names the kernel is on duty to
    resolve them.  Apply same fix here.

--- sys/netpfil/pf/pf_if.c
+++ sys/netpfil/pf/pf_if.c
@@ -559,7 +559,8 @@ pfi_instance_add(struct ifnet *ifp, int net, int flags)
 		}
 		if (af == AF_INET)
 			got4 = 1;
-		else if (af == AF_INET6)
+		else if (af == AF_INET6 && !IN6_IS_ADDR_LINKLOCAL(
+		    &((struct sockaddr_in6 *)ia->ifa_addr)->sin6_addr))
 			got6 = 1;
 		net2 = net;
 		if (net2 == 128 && (flags & PFI_AFLAG_NETWORK)) {
