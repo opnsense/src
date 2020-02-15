efbc33166ca6c238720d88996eebdef069981ce9
vmaffione <vmaffione@FreeBSD.org>
2020-02-15 11:42:13 +0000

    MFC r357663
    netmap: improve netmap(4) and vale(4) man pages
    
    Clean up obsolete sysctl descriptions and add missing ones.
    
    PR:             243838
    Reviewed by:    bcr
    Differential Revision:  https://reviews.freebsd.org/D23546

--- sys/dev/netmap/netmap_kern.h
+++ sys/dev/netmap/netmap_kern.h
@@ -1609,7 +1609,6 @@ int netmap_adapter_put(struct netmap_adapter *na);
 #define NETMAP_BUF_BASE(_na)	((_na)->na_lut.lut[0].vaddr)
 #define NETMAP_BUF_SIZE(_na)	((_na)->na_lut.objsize)
 extern int netmap_no_pendintr;
-extern int netmap_mitigate;
 extern int netmap_verbose;
 #ifdef CONFIG_NETMAP_DEBUG
 extern int netmap_debug;		/* for debugging */
@@ -1631,7 +1630,6 @@ enum {                                  /* debug flags */
 };
 
 extern int netmap_txsync_retry;
-extern int netmap_flags;
 extern int netmap_generic_hwcsum;
 extern int netmap_generic_mit;
 extern int netmap_generic_ringsize;
