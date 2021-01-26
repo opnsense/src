783f4e8016ce272246863b8d85076c4433f773d7
xorbital <53773184+xorbital@users.noreply.github.com>
2021-01-26 14:40:40 +0100

    Update Unbound Documentation URL

--- contrib/unbound/dnscrypt/dnscrypt.c
+++ contrib/unbound/dnscrypt/dnscrypt.c
@@ -870,7 +870,7 @@ sodium_misuse_handler(void)
 		"dnscrypt: libsodium could not be initialized, this typically"
 		" happens when no good source of entropy is found. If you run"
 		" unbound in a chroot, make sure /dev/urandom is available. See"
-		" https://www.unbound.net/documentation/unbound.conf.html");
+		" https://nlnetlabs.nl/documentation/unbound/unbound.conf/");
 }
 
 
