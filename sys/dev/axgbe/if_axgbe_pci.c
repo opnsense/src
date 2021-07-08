e568dbdc79b44cc4637d50633a6834cc22238e9a
Stephan de Wit <swit2@avans.nl>
2021-07-08 11:45:42 +0000

    axgbe: Make sure the drivers works on the V1000 platform and remove unnecessary reset

--- sys/dev/axgbe/if_axgbe_pci.c
+++ sys/dev/axgbe/if_axgbe_pci.c
@@ -392,6 +392,7 @@ axgbe_if_attach_pre(if_ctx_t ctx)
 	if_softc_ctx_t		scctx;
 	if_shared_ctx_t		sctx;
 	device_t		dev;
+	device_t		rdev;
 	unsigned int		ma_lo, ma_hi;
 	unsigned int		reg;
 
@@ -430,6 +431,16 @@ axgbe_if_attach_pre(if_ctx_t ctx)
         sc->pdata.xpcs_res = mac_res[1];
 
         /* Set the PCS indirect addressing definition registers*/
+	rdev = pci_find_dbsf(0, 0, 0, 0);
+	if (rdev && pci_get_device(rdev) == 0x15d0
+		&& pci_get_vendor(rdev) == 0x1022) {
+		pdata->xpcs_window_def_reg = PCS_V2_RV_WINDOW_DEF;
+		pdata->xpcs_window_sel_reg = PCS_V2_RV_WINDOW_SELECT;
+	} else {
+		pdata->xpcs_window_def_reg = PCS_V2_WINDOW_DEF;
+		pdata->xpcs_window_sel_reg = PCS_V2_WINDOW_SELECT;
+	}
+
 	pdata->xpcs_window_def_reg = PCS_V2_WINDOW_DEF;
 	pdata->xpcs_window_sel_reg = PCS_V2_WINDOW_SELECT;
 
