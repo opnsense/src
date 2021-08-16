3dc68ec7f0c802e49ba499ff7482b130acddb2fc
Franco Fichtner <franco@opnsense.org>
2021-08-16 08:10:42 +0200

    bsdinstall: switch branding where appropriate

--- usr.sbin/bsdinstall/distfetch/distfetch.c
+++ usr.sbin/bsdinstall/distfetch/distfetch.c
@@ -69,7 +69,7 @@ main(void)
 	}
 
 	init_dialog(stdin, stdout);
-	dialog_vars.backtitle = __DECONST(char *, "FreeBSD Installer");
+	dialog_vars.backtitle = __DECONST(char *, "OPNsense Installer");
 	dlg_put_backtitle();
 
 	for (i = 0; i < ndists; i++) {
