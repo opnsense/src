96acb4886c923452e79ae4be8d1a5c0f7d2605ba
Franco Fichtner <franco@lastsummer.de>
2021-10-29 09:11:10 +0200

    Update sys/netinet/tcp_hpts.c

--- sys/netinet/tcp_hpts.c
+++ sys/netinet/tcp_hpts.c
@@ -1191,7 +1191,7 @@ hpts_cpuid(struct inpcb *inp, int *failed)
 		return(inp->inp_irq_cpu);
 	}
 	/* If one is set the other must be the same */
-#ifdef	RSS
+#ifdef RSS
 	if (rss_get_enabled()) {
 	cpuid = rss_hash2cpuid(inp->inp_flowid, inp->inp_flowtype);
 	if (cpuid == NETISR_CPUID_NONE)
