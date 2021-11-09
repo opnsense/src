bc99862101aaf7b6496a29ab811e3f54d68702fb
Ad Schellevis <ad@opnsense.org>
2021-11-09 17:21:43 +0100

    ixgbe: workaround to prevent an i2c bus read to keep trying to read an empty slot
    When executing `ifconfig -v` this will lead to stalls for a second per
    interface due to the timeout being set to a static 10 without a module
    placed, this patch makes sure this is only allowed once per insertion.

--- sys/dev/ixgbe/ixgbe_type.h
+++ sys/dev/ixgbe/ixgbe_type.h
@@ -4260,6 +4260,8 @@ struct ixgbe_hw {
 	bool allow_unsupported_sfp;
 	bool wol_enabled;
 	bool need_crosstalk_fix;
+	/* XXX flag for workaround to prevent reading an sfp[+] slot with nothing connected to it. */
+	bool sfp_probe_timed_out;
 	u32 fw_rst_cnt;
 };
 
