616443270769f263edbf84645cc646915d86f3bb
Dimitry Andric <dim@FreeBSD.org>
2022-07-25 20:53:32 +0200

    Adjust function definitions in if_pfsync.c to avoid clang 15 warnings
    With clang 15, the following -Werror warnings are produced:
    
        sys/netpfil/pf/if_pfsync.c:2439:21: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        pfsync_pointers_init()
                            ^
                             void
        sys/netpfil/pf/if_pfsync.c:2453:23: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        pfsync_pointers_uninit()
                              ^
                               void
        sys/netpfil/pf/if_pfsync.c:2503:12: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        pfsync_init()
                   ^
                    void
        sys/netpfil/pf/if_pfsync.c:2524:14: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        pfsync_uninit()
                     ^
                      void
    
    This is because pfsync_pointers_init(), pfsync_pointers_uninit(),
    pfsync_init(), and pfsync_uninit() are declared with (void) argument
    lists, but defined with empty argument lists. Make the definitions match
    the declarations.
    
    MFC after:	3 days
    
    (cherry picked from commit 766f3c8032a95f344823bea70bb7f794f9939d33)

--- sys/netpfil/pf/if_pfsync.c
+++ sys/netpfil/pf/if_pfsync.c
@@ -2437,7 +2437,7 @@ static struct protosw in_pfsync_protosw = {
 #endif
 
 static void
-pfsync_pointers_init()
+pfsync_pointers_init(void)
 {
 
 	PF_RULES_WLOCK();
@@ -2451,7 +2451,7 @@ pfsync_pointers_init()
 }
 
 static void
-pfsync_pointers_uninit()
+pfsync_pointers_uninit(void)
 {
 
 	PF_RULES_WLOCK();
@@ -2501,7 +2501,7 @@ VNET_SYSUNINIT(vnet_pfsync_uninit, SI_SUB_PROTO_FIREWALL, SI_ORDER_FOURTH,
     vnet_pfsync_uninit, NULL);
 
 static int
-pfsync_init()
+pfsync_init(void)
 {
 #ifdef INET
 	int error;
@@ -2522,7 +2522,7 @@ pfsync_init()
 }
 
 static void
-pfsync_uninit()
+pfsync_uninit(void)
 {
 	pfsync_detach_ifnet_ptr = NULL;
 
