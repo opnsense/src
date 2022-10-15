adb8c3eda63e9b81a754b14765340a458bc14aab
Zhenlei Huang <zlei.huang@gmail.com>
2022-10-15 17:05:36 +0200

    if_me: Use dedicated network privilege
    Separate if_me privileges from if_gif.
    
    Reviewed by:	kp
    Approved by:	kp (mentor)
    Differential Revision:	https://reviews.freebsd.org/D36691
    
    (cherry picked from commit 43f8c763cdeea29f95b6f0eebce3ad80dd210c7a)

--- sys/kern/kern_jail.c
+++ sys/kern/kern_jail.c
@@ -3455,6 +3455,7 @@ prison_priv_check(struct ucred *cred, int priv)
 	case PRIV_NET_GIF:
 	case PRIV_NET_SETIFVNET:
 	case PRIV_NET_SETIFFIB:
+	case PRIV_NET_ME:
 
 		/*
 		 * 802.11-related privileges.
