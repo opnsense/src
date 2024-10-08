304cf69371689e4a9a5b22f79cb2c5008a142137
Franco Fichtner <franco@opnsense.org>
2024-10-08 11:01:47 +0200

    libfetch: ignore the error of an absence of a CRL
    ... when passing SSL_CRL_FILE / SSL_CRL_VERIFY.
    
    The situation isn't ideal, but since we don't know what we are going to deal
    with the situation is tricky.  It's especially pointless in scenarios of
    pkg multi-repo cases where we need to deal wit a mixed bag of URLs during
    the same context.  For the benefit of the doubt print the appropriate message
    for the user to see.
    
    In general it would be a bit safer if we could enforce the existence of a
    CRL distribution point as a mandatory CRL check and the others as an optional
    one with the warning as printed for the user to see.  It would also need a
    strict mode if someone needed the other behaviour but since we did not have
    any consumers of SSL_CRL_FILE and --crl was broken for a long time it's safe
    to assume nobody uses this for these specific reasons.

--- lib/libfetch/common.c
+++ lib/libfetch/common.c
@@ -1161,6 +1161,27 @@ fetch_ssl_cb_verify_crt(int verified, X509_STORE_CTX *ctx)
 	char *str;
 
 	str = NULL;
+
+	/*
+	 * CLRs may be defined explictly but not always.  The absence of a
+	 * CRL distribution point is no indication that a CRL does not exist
+	 * which also means the CRL check being enforced will require to have
+	 * proper CRLs in place for the certificates to be checked which can
+	 * not be guaranteed for a random hostname on the Internet.  Disable
+	 * the specific OpenSSL error that deals with this case given that
+	 * we do make the utmost effort to supply a proper list of CRLs that
+	 * are required to verify the certificate(s) in question.
+	 */
+	if (!verified && X509_STORE_CTX_get_error(ctx) == X509_V_ERR_UNABLE_TO_GET_CRL) {
+		if ((crt = X509_STORE_CTX_get_current_cert(ctx)) != NULL &&
+		    (name = X509_get_subject_name(crt)) != NULL)
+			str = X509_NAME_oneline(name, 0, 0);
+		fetch_info("No CRL was provided for %s", str);
+		OPENSSL_free(str);
+
+		verified = 1;
+	}
+
 	if (!verified) {
 		if ((crt = X509_STORE_CTX_get_current_cert(ctx)) != NULL &&
 		    (name = X509_get_subject_name(crt)) != NULL)
@@ -1169,6 +1190,7 @@ fetch_ssl_cb_verify_crt(int verified, X509_STORE_CTX *ctx)
 		    str != NULL ? str : "no relevant certificate");
 		OPENSSL_free(str);
 	}
+
 	return (verified);
 }
 
