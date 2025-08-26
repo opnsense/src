a4baf3080020e14e829dc84a8598e96dcd40b114
Franco Fichtner <franco@opnsense.org>
2025-08-26 12:42:26 +0200

    libfetch: restructure this code like it was originally intended #261
    Some back and forth but this makes the initial submission a little better.

--- lib/libfetch/common.c
+++ lib/libfetch/common.c
@@ -1172,18 +1172,20 @@ fetch_ssl_cb_verify_crt(int verified, X509_STORE_CTX *ctx)
 	 * we do make the utmost effort to supply a proper list of CRLs that
 	 * are required to verify the certificate(s) in question.
 	 */
-	if (!verified && X509_STORE_CTX_get_error(ctx) == X509_V_ERR_UNABLE_TO_GET_CRL) {
-		if ((crt = X509_STORE_CTX_get_current_cert(ctx)) != NULL &&
-		    (name = X509_get_subject_name(crt)) != NULL) {
-			if (X509_STORE_CTX_get_error_depth(ctx) != 0) {
+	if (!verified && X509_STORE_CTX_get_error(ctx) ==X509_V_ERR_UNABLE_TO_GET_CRL) {
+		if (X509_STORE_CTX_get_error_depth(ctx) != 0) {
+			if ((crt = X509_STORE_CTX_get_current_cert(ctx)) != NULL &&
+			    (name = X509_get_subject_name(crt)) != NULL)
 				str = X509_NAME_oneline(name, 0, 0);
-				fetch_info("No CRL was provided for CA %s", str);
-				OPENSSL_free(str);
-			}
+			fetch_info("No CRL was provided for CA %s",
+			    str != NULL ? str : "no relevant certificate");
+			OPENSSL_free(str);
 		}
 
 		verified = 1;
 	}
+
+	str = NULL;
 
 	if (!verified) {
 		if ((crt = X509_STORE_CTX_get_current_cert(ctx)) != NULL &&
