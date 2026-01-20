da4b2cd01b4002c81d180dc7a8d482294e412c17
Franco Fichtner <franco@opnsense.org>
2026-01-20 09:19:33 +0100

    Revert "LinuxKPI: rename from_timer() to timer_container_of()"
    This reverts commit 336be73ac1e6b26b9163460f0e88432508eb8394.
    
    This breaks building rtw88.

--- sys/compat/linuxkpi/common/src/linux_80211.c
+++ sys/compat/linuxkpi/common/src/linux_80211.c
@@ -7417,7 +7417,7 @@ lkpi_wiphy_delayed_work_timer(struct timer_list *tl)
 {
 	struct wiphy_delayed_work *wdwk;
 
-	wdwk = timer_container_of(wdwk, tl, timer);
+	wdwk = from_timer(wdwk, tl, timer);
 	wiphy_work_queue(wdwk->wiphy, &wdwk->work);
 }
 
