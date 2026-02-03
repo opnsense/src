f19fe1bd865eb5a1ffb859ef0d869ba56904b715
Enji Cooper <ngie@FreeBSD.org>
2026-02-02 23:35:24 -0800

    OpenSSL: update build artifacts to match 3.0.16 release
    The files committed match the output of the new vendor process. Much of
    this involves regenerating manpages to catch up to content from the
    initial 3.0 import.
    
    This is a direct commit to stable/14.

--- crypto/openssl/include/openssl/configuration.h
+++ crypto/openssl/include/openssl/configuration.h
@@ -34,6 +34,9 @@ extern "C" {
 # ifndef OPENSSL_THREADS
 #  define OPENSSL_THREADS
 # endif
+# ifndef OPENSSL_NO_ACVP_TESTS
+#  define OPENSSL_NO_ACVP_TESTS
+# endif
 # ifndef OPENSSL_NO_AFALGENG
 #  define OPENSSL_NO_AFALGENG
 # endif
@@ -49,16 +52,14 @@ extern "C" {
 # ifndef OPENSSL_NO_CRYPTO_MDEBUG_BACKTRACE
 #  define OPENSSL_NO_CRYPTO_MDEBUG_BACKTRACE
 # endif
-# if !defined(__LP64__) || __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
-#  ifndef OPENSSL_NO_EC_NISTP_64_GCC_128
-#   define OPENSSL_NO_EC_NISTP_64_GCC_128
-#  endif
-# endif
 # ifndef OPENSSL_NO_EGD
 #  define OPENSSL_NO_EGD
 # endif
 # ifndef OPENSSL_NO_EXTERNAL_TESTS
 #  define OPENSSL_NO_EXTERNAL_TESTS
+# endif
+# ifndef OPENSSL_NO_FIPS_SECURITYCHECKS
+#  define OPENSSL_NO_FIPS_SECURITYCHECKS
 # endif
 # ifndef OPENSSL_NO_FUZZ_AFL
 #  define OPENSSL_NO_FUZZ_AFL
@@ -123,21 +124,11 @@ extern "C" {
  * The following are cipher-specific, but are part of the public API.
  */
 # if !defined(OPENSSL_SYS_UEFI)
-#  if __SIZEOF_LONG__ == 8
-#   undef BN_LLONG
+#  undef BN_LLONG
 /* Only one for the following should be defined */
-#   define SIXTY_FOUR_BIT_LONG
-#   undef SIXTY_FOUR_BIT
-#   undef THIRTY_TWO_BIT
-#  elif __SIZEOF_LONG__ == 4
-#   define BN_LLONG
-/* Only one for the following should be defined */
-#   undef SIXTY_FOUR_BIT_LONG
-#   undef SIXTY_FOUR_BIT
-#   define THIRTY_TWO_BIT
-#  else
-#   error Unsupported size of long
-#  endif
+#  define SIXTY_FOUR_BIT_LONG
+#  undef SIXTY_FOUR_BIT
+#  undef THIRTY_TWO_BIT
 # endif
 
 # define RC4_INT unsigned int
@@ -147,3 +138,41 @@ extern "C" {
 # endif
 
 #endif                          /* OPENSSL_CONFIGURATION_H */
+
+/**
+ * OpenSSL's Configure script generates these values automatically for the host
+ * architecture, but FreeBSD provides values which are universal for all
+ * supported target architectures.
+ */
+
+#ifndef	__FREEBSD_CONFIGURATION_H__
+#define	__FREEBSD_CONFIGURATION_H__
+
+# undef OPENSSL_NO_EC_NISTP_64_GCC_128
+# if __SIZEOF_LONG__ == 4 || __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
+#  ifndef OPENSSL_NO_EC_NISTP_64_GCC_128
+#   define OPENSSL_NO_EC_NISTP_64_GCC_128
+#  endif
+# endif
+
+# undef BN_LLONG
+# undef	SIXTY_FOUR_BIT_LONG
+# undef SIXTY_FOUR_BIT
+# undef	THIRTY_TWO_BIT
+# if !defined(OPENSSL_SYS_UEFI)
+#  if __SIZEOF_LONG__ == 8
+#   undef BN_LLONG
+#   define SIXTY_FOUR_BIT_LONG
+#   undef SIXTY_FOUR_BIT
+#   undef THIRTY_TWO_BIT
+#  elif __SIZEOF_LONG__ == 4
+#   define BN_LLONG
+#   undef SIXTY_FOUR_BIT_LONG
+#   undef SIXTY_FOUR_BIT
+#   define THIRTY_TWO_BIT
+#  else
+#   error Unsupported size of long
+#  endif
+# endif
+
+#endif  /* __FREEBSD_CONFIGURATION_H__ */
